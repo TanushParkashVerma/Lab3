a767b869abb14f0f507b8de4f0fe12caefba80d6
TanushParkashVerma <tanush.parkash16@gmail.com>
2019-09-27 20:38:02 -0700

    another unit test added

--- unit_tests.cpp
+++ unit_tests.cpp
@@ -9,14 +9,28 @@
 
 TEST_CASE("A new stack is empty", "testTag1")
 {
-Stack tester;
-REQUIRE(tester.empty() == true);
-REQUIRE(tester.full() == false);
+Stack tester1;
+REQUIRE(tester1.empty() == true);
+REQUIRE(tester1.full() == false);
 }
 
 TEST_CASE("A new int is pushed to stack or not ", "testTag2")
 {
+Stack tester2;
+REQUIRE(tester2.push(2) == true);
+REQUIRE(tester2.push(3) == true);
+}
+
+TEST_CASE("A new stack is full or not", "testTag3")
+{
 Stack tester;
-REQUIRE(tester.push(2) == true);
-REQUIRE(tester.push(3) == true);
+REQUIRE(tester.full() == false);
+REQUIRE(tester.full() == false);
+}
+
+TEST_CASE("to check top of stack element", "testTag4")
+{
+Stack tester3;
+REQUIRE(tester3.top() == 1);
+REQUIRE(tester3.top() == 1);
 }
